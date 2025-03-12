e4ecb75dd62cbf41689898534ad1d9188051e62e
Jérémy SERSOUB <116919971+jeremsers@users.noreply.github.com>
2025-03-12 14:29:25 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `
-                  @use "/src/assets/style/utils.scss" as *;
-                `
-          }
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+                @use "/src/assets/style/utils.scss" as *;
+              `
         }
       }
-    },
-  });
+    }
+  },
+  site:"https://www.essence-et-conscience.ch",
+
+  integrations: [sitemap()]
+});
