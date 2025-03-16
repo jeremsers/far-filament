ff9e3fdc73d5e38aa4939060bd14aed1dafd677b
Jérémy SERSOUB <116919971+jeremsers@users.noreply.github.com>
2025-03-16 13:48:22 +0100

    rev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: vercel(),
 	vite: {
 		css: {
 			preprocessorOptions: {
